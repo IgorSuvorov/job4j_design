3298cabbf2a7ce00c8641cd4319c33525f9c4480
isuvorov1 <isuvorov1@outlook.com>
2021-12-20 14:09:01 -0500

    io

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 import java.io.FileOutputStream;
 
-
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
@@ -14,6 +13,5 @@ public class ResultFile {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
